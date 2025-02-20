a25a7beeae497654d4ee286d8e80c559251b459d
Alyson Felipe Ozório <alysoonfelipe@hotmail.com>
2025-02-20 16:23:24 -0300

    adicionando tags no componente Body

--- src/Body.js
+++ src/Body.js
@@ -1,9 +1,15 @@
+import Stories from "./Stories";
+import Posts from "./Posts";
+import SideBar from "./SideBar";
+
 export default function(){
     return (
         <div class="corpo">
             <div class="esquerda">
-                
+                <Stories />
+                <Posts />
+                <SideBar />
             </div>
         </div>
-    )
+    );
 }
