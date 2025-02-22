c82d24f56529a84ca55b307f06b5b07ed46c5f91
Alyson Felipe Ozório <alysoonfelipe@hotmail.com>
2025-02-22 12:29:02 -0300

    Adicionado opção de mudar nome e imagem do usuario

--- src/User.js
+++ src/User.js
@@ -1,11 +1,36 @@
+import React from "react"
+
 export default function User(){
+    const [usuario, setUsuario] = React.useState("catanacomics");
+    const [imgUsuario, setImgUsuario] = React.useState("assets/catanacomics.svg");
+
+    function alterarUsuario(){
+        const perguntaNome = prompt("Qual seu nome?");
+
+        if(!usuario){
+            setUsuario(usuario);
+        } else if(usuario) {
+            setUsuario(perguntaNome);
+        }
+    }
+
+    function alterarImagem(){
+        const perguntaImagem = prompt("Adicione aqui o link de sua imagem");
+
+    if(!imgUsuario){
+            setImgUsuario(imgUsuario)
+        } else if(imgUsuario){
+            setImgUsuario(perguntaImagem);
+        }
+    }
+
     return (
         <div className="usuario">
-            <img src="assets/catanacomics.svg" alt="imagem de perfil"/>
+            <img src={imgUsuario} alt="imagem de perfil" onClick={(() => alterarImagem())}/>
             <div className="texto">
                 <span>
-                <strong>catanacomics</strong>
-                <ion-icon name="pencil"></ion-icon>
+                <strong>{usuario}</strong>
+                <ion-icon name="pencil" onClick={(() => alterarUsuario())}></ion-icon>
                 </span>
             </div>
         </div>
